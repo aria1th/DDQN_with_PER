e3641f5d40130ea52e8f8b0e88fa3d014cab36d5
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2022-06-20 21:16:40 +0900

    Add files via upload

---
+++ per.py
@@ -0,0 +1,60 @@
+import random
+try:
+    import sumtree
+except:
+    from . import sumtree
+import numpy as np
+
+
+class PrioritizedMemory:
+    e = 0.11
+    a = 0.6
+    beta = 0.4
+    beta_increment_per_sampling = 0.001
+    update_gamma = 0.95
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = None # Do not access this directly!
+        self.tree = sumtree.SumTree(capacity) #see https://github.com/rlcode/per/blob/master/prioritized_memory.py
+        self.prior = True
+    def _get_priority(self, error):
+        return (np.abs(error) + self.e) ** self.a
+
+    def push(self, error, state, action, reward, state_next, done): 
+        #error is reward change so we need to calculate
+        p = self._get_priority(error)
+        self.tree.add(p, (state, action, state_next, reward, done))
+    def sample(self, batch_size):
+        batch = []
+        idxs = []
+        segment = self.tree.total() / batch_size
+        priorities = []
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
+        for i in range(batch_size):
+            a = segment * i
+            b = segment * (i+1)
+            s = random.uniform(a,b)
+            (idx, p, data) = self.tree.get(s)
+            if data == 0:
+                s = random.uniform(0, a)
+                (idx, p, data) = self.tree.get(s)
+            priorities.append(p)
+            batch.append(data)
+            idxs.append(idx)
+        sampling_prob = priorities / self.tree.total()
+        is_w = np.power(self.tree.n_entries * sampling_prob, -self.beta)
+        if is_w.max() != 0: is_w /= is_w.max() #no DivisionByZero
+        return batch, idxs, is_w
+    def __len__(self):
+        return len(self.tree)
+    def update(self, idx, error):
+        p = self._get_priority(error)
+        self.tree.update(idx, p, self.update_gamma)
+    def clear(self):
+        self.capacity = capacity
+        self.memory = None # Do not access this directly!
+        del self.tree
+        self.tree = sumtree.SumTree(capacity) #see https://github.com/rlcode/per/blob/master/prioritized_memory.py
+        self.prior = True
+        self.beta = PrioritizedMemory.beta
